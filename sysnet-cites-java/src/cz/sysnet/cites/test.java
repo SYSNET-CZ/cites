8d171476e97b6ec2c6f01e6a6c1e91f084badfa6
Radim Jäger <rjaeger@sysnet.cz>
2014-10-22 16:42:42 +0200

    Uvodni commit

---
+++ sysnet-cites-java/src/cz/sysnet/cites/test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package cz.sysnet.cites;
+
+/**
+ * @author Radim
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ahoj, swěte!");
+
+	}
+
+}
