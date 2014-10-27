8057c4c3b1a53751d73b414fef8ca0f7bf65c1e0
Radim Jäger <rjaeger@sysnet.cz>
2014-10-27 07:19:19 +0100

    9

--- sysnet-cites-java/src/cz/sysnet/cites/PdfFactory.java
+++ sysnet-cites-java/src/cz/sysnet/cites/PdfFactory.java
@@ -1,15 +1,26 @@
 package cz.sysnet.cites;
 
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.Serializable;
 import java.nio.charset.Charset;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.*;
 
@@ -22,13 +33,15 @@ import com.itextpdf.text.pdf.*;
 public class PdfFactory implements Serializable {
 	private static final String BEAN_NAME = "PdfFactory";
 	private static final long serialVersionUID = 6567844958570848594L;
-	private static Charset CS = Charset.forName("UTF-8");
+	private static final Charset CS = Charset.forName("UTF-8");
+	private static final String EOL = System.getProperty("line.separator");  
 	private WorkDocument doc;
 	private boolean debug;
 	private PdfReader form;
 	private XfdfReader data;
 	private XfaForm xfa;
 	private PdfStamper outPdf;
+	private Map<String, PdfTemplate> template;
 	
 	public PdfFactory() {
 		this.doc = new WorkDocument(new IdentFactory());
@@ -45,6 +58,49 @@ public class PdfFactory implements Serializable {
 		this.data = null;
 		this.xfa = null;
 		this.outPdf = null;		
+	}
+	
+	public void loadTemplates(String sourceUrl) throws IllegalStateException, IOException {
+		HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(sourceUrl);
+        HttpResponse response = client.execute(request);
+        BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+        String json = "";
+        String line = "";
+        String baseurl = request.getURI().toURL().getProtocol() + "://" + request.getURI().toURL().getHost();
+        while ((line = rd.readLine()) != null) { 
+        	json += line.trim() + EOL;
+        }
+        json = json.trim();
+        JSONArray jsonArray = JSONArray.fromObject(json);
+        JSONObject jsonObject;
+        for (int i = 0; i < jsonArray.size(); i++ ) {
+        	jsonObject = jsonArray.getJSONObject(i);
+        	String key = jsonObject.getString("id");
+        	PdfTemplate bean = new PdfTemplate(key, jsonObject.getString("name"), jsonObject.getString("filename"), jsonObject.getString("url"));
+        	
+        	bean.setId(jsonObject.getString("id"));
+        	System.out.print("name\t" );
+        	System.out.println(jsonObject.get("name"));
+           	bean.setName(jsonObject.getString("name"));
+            System.out.print("filename\t" );
+        	System.out.println(jsonObject.get("filename"));
+           	bean.setFilename(jsonObject.getString("filename"));
+        	System.out.print("url\t" );
+        	System.out.println(baseurl + jsonObject.get("url"));
+           	bean.setUrl(baseurl + jsonObject.getString("url"));
+           	System.out.print("filepath\t" );
+        	System.out.println(System.getProperty("java.io.tmpdir") + bean.getFilename());
+           	bean.setFilepath(System.getProperty("java.io.tmpdir") + bean.getFilename());
+           	
+           	
+           	
+        	
+        	//bean = JSONObject.toBean( jsonObject );
+        	//bean = (PdfTemplate) JSONObject.toBean( jsonObject, PdfTemplate.class );  
+        }
+        
+        
 	}
 	
 	public void setSourcePath(String sourcePath) throws IOException {
@@ -116,6 +172,14 @@ public class PdfFactory implements Serializable {
 
 	public void setDoc(WorkDocument doc) {
 		this.doc = doc;
+	}
+
+	public Map<String, PdfTemplate> getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(Map<String, PdfTemplate> template) {
+		this.template = template;
 	}
 
 	public static String getBeanName() {
@@ -241,3 +305,4 @@ public class PdfFactory implements Serializable {
 		return outpath;		
 	}
 }
+
--- sysnet-cites-java/src/cz/sysnet/cites/test.java
+++ sysnet-cites-java/src/cz/sysnet/cites/test.java
@@ -2,6 +2,17 @@
  * 
  */
 package cz.sysnet.cites;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONArray;
 
 /**
  * @author Radim
@@ -16,14 +27,67 @@ public class test {
 	static WorkDocument wdoc;
 	static IdentFactory ifact;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalStateException, IOException {
 		// TODO Auto-generated method stub
+		String eol = System.getProperty("line.separator");  
 		
 		ifact = new IdentFactory();
 		wdoc = new WorkDocument(ifact);
 		pfact = new PdfFactory(wdoc);
 		pfact.setDebug(true);
+		
+		//String result = restTemplate.
 		System.out.println("Ahoj, swěte!");
-	}
+		
+		String url = "http://athos.sysnet.cz/clients/env/cites/v3/cites-admin300.nsf/api/data/collections/name/pdf-forms";
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+        HttpResponse response = client.execute(request);
+        BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+        String json = "";
+        String line = "";
+        String baseurl = request.getURI().toURL().getProtocol() + "://" + request.getURI().toURL().getHost();
+      	System.out.println(baseurl);
+      	
+        while ((line = rd.readLine()) != null) {
+        	json += line.trim() + eol;
+        	//System.out.println(line);
+        }
+        json = json.trim();
+       
+    	System.out.println("-------------------------------------------------------------------------");
+    	//System.out.println(json);
 
+        //JSONObject jsonObject = JSONObject.fromObject( json );
+        JSONArray jsonArray = JSONArray.fromObject(json);
+        JSONObject jsonObject;
+        //Object bean;
+        PdfTemplate bean = new PdfTemplate();
+        for (int i = 0; i < jsonArray.size(); i++ ) {
+        	jsonObject = jsonArray.getJSONObject(i);
+        	System.out.print("id\t" );
+        	System.out.println(jsonObject.get("id"));
+        	bean.setId(jsonObject.getString("id"));
+        	System.out.print("name\t" );
+        	System.out.println(jsonObject.get("name"));
+           	bean.setName(jsonObject.getString("name"));
+            System.out.print("filename\t" );
+        	System.out.println(jsonObject.get("filename"));
+           	bean.setFilename(jsonObject.getString("filename"));
+        	System.out.print("url\t" );
+        	System.out.println(baseurl + jsonObject.get("url"));
+           	bean.setUrl(baseurl + jsonObject.getString("url"));
+           	System.out.print("filepath\t" );
+        	System.out.println(System.getProperty("java.io.tmpdir") + bean.getFilename());
+           	bean.setFilepath(System.getProperty("java.io.tmpdir") + bean.getFilename());
+           	
+           	
+           	
+        	
+        	//bean = JSONObject.toBean( jsonObject );
+        	//bean = (PdfTemplate) JSONObject.toBean( jsonObject, PdfTemplate.class );  
+        }
+        
+        System.out.println("Ahoj, swěte! " + jsonArray.size());
+	}
 }
